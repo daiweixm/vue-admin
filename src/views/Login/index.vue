826691d6f6c43045c27cfad1ec40ed2960421cc1
daiweixm <daiweixm@gmail.com>
2021-03-03 19:45:41 +0800

    add login

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,16 +1,56 @@
 <template>
   <div id="login">
-    <el-button>default</el-button>
+    <div class="login-wrap">
+      <ul class="menu-tab">
+        <li
+          v-for="item in menuTab"
+          :key="item.index"
+          :class="{ current: isActive }"
+        >
+          {{ item.txt }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
 export default {
-  name: "login"
+  name: "login",
+  data() {
+    return {
+      menuTab: [{ txt: "登录" }, { txt: "注册" }],
+      isActive: false
+    };
+  },
+  created() {},
+  mounted() {},
+  methods: {},
+  props: {},
+  watch: {}
 };
 </script>
 <style lang="scss" scoped>
 #login {
   height: 100vh;
   background-color: #344a5f;
+}
+.login-wrap {
+  width: 330px;
+  margin: auto;
+}
+.menu-tab {
+  text-align: center;
+  li {
+    display: inline-block;
+    width: 88px;
+    line-height: 36px;
+    font-size: 14px;
+    color: #fff;
+    border-radius: 2px;
+    cursor: pointer;
+  }
+  .current {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,12 @@
-const path = require('path')
-module.exports={
-  publicPath: process.env.NODE_ENV === 'production' ? '':'/',
-  outputDir: process.env.NODE_ENV === 'production' ? 'dist':'devdist',
+const path = require("path");
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "" : "/",
+  outputDir: process.env.NODE_ENV === "production" ? "dist" : "devdist",
   lintOnSave: true,
-  chainWebpack: (config) => {
-  },
-  configureWebpack: (config) => {
-  },
+  chainWebpack: (config) => {},
+  configureWebpack: (config) => {},
   productionSourceMap: false,
-  css:{
+  css: {
     extract: true,
     sourceMap: false,
     loaderOptions: {
@@ -20,10 +18,10 @@ module.exports={
     modules: false
   },
   // 是否为 Babel 或 TypeScript 使用 thread-loader
-  parallel: require('os').cpus().length > 1,
+  parallel: require("os").cpus().length > 1,
   pwa: {},
   // 所有 webpack-dev-server 的选项都支持
   devServer: {},
-   // 可以用来传递任何第三方插件选项
-   pluginOptions: {}
-}
+  // 可以用来传递任何第三方插件选项
+  pluginOptions: {}
+};
